2bc9cdaa9a44150f608bb8aa67d31c25fd87423e
cd-x <whowantsthisemailid@gmail.com>
2023-05-22 23:19:25 +0530

    form complete

--- HelloWorld/ContentView.swift
+++ HelloWorld/ContentView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var birthDate = Date()
+    @State private var shouldSendNewLetter = true
+    @State private var numberOfLikes = 1
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            Form{
+                Section(header:  Text("Personal Information")){
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name", text: $lastName)
+                    DatePicker("Birthdate", selection: $birthDate, displayedComponents: .date)
+                }
+                Section(header: Text("Actions")){
+                    Toggle("Send news letter", isOn: $shouldSendNewLetter)
+                        .toggleStyle(SwitchToggleStyle(tint: .red))
+                    Stepper("Number of likes", value: $numberOfLikes, in: 1...100)
+                    Text("This video has \(numberOfLikes) likes")
+                    Link("Terms Of Service", destination: URL(string: "https://cd-x.github.io")!)
+                }
+            }
+            .navigationTitle("Account")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+            .previewInterfaceOrientation(.portrait)
+            
     }
 }
