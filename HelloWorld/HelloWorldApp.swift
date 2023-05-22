123b47fa8198b14c02d1e12b34f3381111ae9174
cd-x <whowantsthisemailid@gmail.com>
2023-05-22 20:23:00 +0530

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Rajrishi Vishwakarma on 22/05/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
